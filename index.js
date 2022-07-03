af7daaa16182ff176d1b7952211feaeafd9390ba
caudillografico <caudillografico@gmail.com>
2022-07-02 23:12:47 -0500

    Modulo2Unidad2 trabajo de Jorge Gaitan

---
+++ index.js
@@ -0,0 +1,13 @@
+const 
+a = document.getElementById('num1');
+b = document.getElementById('num2');
+c = document.getElementById('num3');
+d = document.getElementById('num4');
+boton = document.getElementById('boton');
+numMayor = document.getElementById('resultadoMayor');
+numMenor = document.getElementById('resultadoMenor');
+
+boton.addEventListener('click', () => {
+    if ((a.value ==)) {
+        
+    }
